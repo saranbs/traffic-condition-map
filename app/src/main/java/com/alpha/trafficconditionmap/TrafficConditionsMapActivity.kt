519ef65d7d16e78ca84edb6cfde7c119748200fa
saranbs <saranshajibabu@gmail.com>
2020-09-12 10:53:19 +0800

    Base setup

---
+++ app/src/main/java/com/alpha/trafficconditionmap/TrafficConditionsMapActivity.kt
@@ -0,0 +1,12 @@
+package com.alpha.trafficconditionmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrafficConditionsMapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
